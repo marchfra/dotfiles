90d152b51a74c94b219057784bdc4012a546512a
Francesco Marchisotti <francesco.marchisotti@gmail.com>
2026-01-07 15:00:13 +0100

    add prettier as json formatter

--- nvim/.config/nvim/lua/plugins/extend-conform.lua
+++ nvim/.config/nvim/lua/plugins/extend-conform.lua
@@ -3,7 +3,8 @@ return {
     "stevearc/conform.nvim",
     opts = {
       formatters_by_ft = {
-        ["markdown"] = { "prettier" },
+        markdown = { "prettier" },
+        json = { "prettier" },
       },
     },
   },
